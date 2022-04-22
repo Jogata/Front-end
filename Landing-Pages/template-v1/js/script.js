7a8723fc96220017250bc84c9fca5b76425e33f0
Yonko Nikolov <jonkoslav@abv.bg>
2022-04-22 14:31:34 +0300

    script

--- Landing-Pages/template-v1/js/script.js
+++ Landing-Pages/template-v1/js/script.js
@@ -1,1 +1,25 @@
+const signupModal = document.querySelector('.signup-form-wrapper');
+const loginModal = document.querySelector('.login-form-wrapper');
+const signupBtn = document.querySelector('.signup-btn');
+const loginBtn = document.querySelector('.login-btn');
+const signupX = document.querySelector('.signup-x');
+const loginX = document.querySelector('.login-x');
+const formContainer = document.querySelector('.form-container');
 
+signupBtn.addEventListener('click', () => {
+    signupModal.classList.remove('hidden');
+    formContainer.classList.toggle('hidden');
+})
+loginBtn.addEventListener('click', () => {
+    loginModal.classList.remove('hidden');
+    formContainer.classList.toggle('hidden');
+})
+
+signupX.addEventListener('click', () => {
+    signupModal.classList.add('hidden');
+    formContainer.classList.toggle('hidden');
+})
+loginX.addEventListener('click', () => {
+    loginModal.classList.add('hidden');
+    formContainer.classList.toggle('hidden');
+})
